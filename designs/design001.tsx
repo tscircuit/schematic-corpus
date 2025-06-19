2bf4fe0c8ebc3627f3227d4b0c6d98ee7f36ae39
seveibar <seveibar@gmail.com>
2025-06-18 22:01:00 -0700

    bootstrap repo

---
+++ designs/design001.tsx
@@ -0,0 +1,41 @@
+export default () => (
+  <board routingDisabled>
+    <chip
+      name="U1"
+      manufacturerPartNumber="I2C_SENSOR"
+      footprint="soic4"
+      pinLabels={{
+        pin1: "SCL",
+        pin2: "SDA",
+        pin3: "VCC",
+        pin4: "GND",
+      }}
+      schPinArrangement={{
+        leftSide: {
+          direction: "top-to-bottom",
+          pins: ["SCL", "SDA", "VCC", "GND"],
+        },
+      }}
+      connections={{
+        SCL: sel.net.SCL,
+        SDA: sel.net.SDA,
+        VCC: sel.net.V3_3,
+        GND: sel.net.GND,
+      }}
+    />
+    <netlabel
+      schX={-2}
+      schY={-1}
+      anchorSide="top"
+      net="GND"
+      connection="U1.GND"
+    />
+    <netlabel
+      schX={-2}
+      schY={0.8}
+      net="VCC"
+      connection="U1.VCC"
+      anchorSide="bottom"
+    />
+  </board>
+)
