05bd464ec06471530e44de246e032f0b4f2b7198
seveibar <seveibar@gmail.com>
2025-06-20 19:12:32 -0700

    more accurate file name

--- scripts/create-bpcs.ts
+++ scripts/create-bpcs.ts
@@ -4,6 +4,6 @@ import { convertCircuitJsonToBpc } from "circuit-json-to-bpc"
 // TODO: go through every dist/**/XXX/circuit.json file in the designs directory
 // and make a dist/**/XXX/bpc.json file
 
-// TODO create a dist/bpc-index.json file that exports a massive JSON object
+// TODO create a dist/bundled-bpc-graphs.json file that exports a massive JSON object
 // that maps each design name to it's bpc.json file e.g.
 // { design001: { boxes: [ ... ], ... }, design002: { boxes: [ ... ], ... }, ... }
