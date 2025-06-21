f8572753cf9dbc152ec57fe03fb7936413ec7cfe
seveibar <seveibar@gmail.com>
2025-06-20 21:42:00 -0700

    add design003

---
+++ designs/design003.circuit.tsx
@@ -0,0 +1,42 @@
+import { sel } from "tscircuit"
+
+export default () => (
+  <board routingDisabled>
+    <jumper
+      name="U1"
+      pinCount={2}
+      connections={{
+        pin2: sel.net().OUT,
+      }}
+      schPinStyle={{
+        pin1: { marginTop: 0.6 },
+      }}
+    />
+    <netlabel
+      schX={2}
+      schY={2}
+      anchorSide="bottom"
+      net="V3_3"
+      connectsTo={sel.U1.pin2}
+    />
+    <capacitor
+      name="C1"
+      schX={2}
+      schY={-1}
+      polarized
+      schRotation="90deg"
+      connections={{
+        pin2: sel.U1.pin2,
+      }}
+      // footprint="0402"
+      capacitance="1uF"
+    />
+    <netlabel
+      schX={2}
+      schY={-2}
+      connectsTo={sel.C1.pin1}
+      net="GND"
+      anchorSide="top"
+    />
+  </board>
+)
