d398ab4021f5d610d8fd3cf67ecb3d27d8a515f6
Ansh Grover <168731971+Anshgrover23@users.noreply.github.com>
2025-06-27 18:58:46 +0530

    add design022 (#24)
    * add design022
    
    * use sel
    
    * use sel

---
+++ designs/design022.circuit.tsx
@@ -0,0 +1,53 @@
+import { sel } from "tscircuit"
+
+export default () => (
+  <board width="10mm" height="10mm">
+    <netlabel
+      schX={6.505}
+      schY={2.5}
+      net="V3_3"
+      connection="PWR.pin2"
+      anchorSide="bottom"
+    />
+    <solderjumper
+      name="PWR"
+      pinCount={2}
+      footprint="solderjumper2_bridged12_p1.041_pw0.660_ph1.270"
+      bridgedPins={[["1", "2"]]}
+      schX={6.505}
+      schY={1.8}
+      schRotation={90}
+      layer="bottom"
+      connections={{
+        pin1: sel.R4.pin2,
+      }}
+    />
+    <resistor
+      resistance="1k"
+      footprint="0603"
+      name="R4"
+      schX={6.505}
+      schY={0.5}
+      schRotation={90}
+      connections={{
+        pin1: sel.D3.pin1,
+      }}
+    />
+    <led
+      name="D3"
+      footprint="0603"
+      schRotation={270}
+      schY={-0.9}
+      schX={6.5}
+      color="red"
+      schDisplayValue="RED"
+    />
+    <netlabel
+      schX={6.505}
+      schY={-1.7}
+      net="GND"
+      anchorSide="top"
+      connection={sel.D3.pin2}
+    />
+  </board>
+)
