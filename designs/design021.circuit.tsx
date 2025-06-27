d05013df515737b6d911cd75d8c825e0904c9462
Anshgrover23 <ag5989670@gmail.com>
2025-06-27 18:57:26 +0530

    use sel

--- designs/design021.circuit.tsx
+++ designs/design021.circuit.tsx
@@ -8,35 +8,35 @@ export default () => (
       schY={-5.6}
       net="VIN"
       anchorSide="bottom"
-      connection="J1.pin1"
+      connection={sel.J1.pin1}
     />
     <netlabel
       schX={0.8}
       schY={-6}
       net="GND"
       anchorSide="top"
-      connectsTo={["J1.pin2", "J1.pin3"]}
+      connectsTo={[sel.J1.pin2, sel.J1.pin3]}
     />
     <netlabel
       schX={0.8}
       schY={-4.2}
       net="V5"
       anchorSide="bottom"
-      connection="J1.pin4"
+      connection={sel.J1.pin4}
     />
     <netlabel
       schX={1.2}
       schY={-4.2}
       net="VDD"
       anchorSide="bottom"
-      connectsTo={["J1.pin5", "J1.pin7"]}
+      connectsTo={[sel.J1.pin5, sel.J1.pin7]}
     />
     <netlabel
       schX={1.4}
       schY={-4.7}
       net="N_RESET"
       anchorSide="left"
-      connection="J1.pin6"
+      connection={sel.J1.pin6}
     />
   </board>
 )
