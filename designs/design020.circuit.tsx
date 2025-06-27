ea7bf0a717e0b00097355c5ef6d3dc2d0ec603a6
techmannih <manishchaudharyttt@gmail.com>
2025-06-27 17:00:17 +0530

    add design020

---
+++ designs/design020.circuit.tsx
@@ -0,0 +1,44 @@
+import { sel } from "tscircuit"
+
+export default () => (
+  <board width="10mm" height="10mm">
+    <chip
+      name="U1"
+      schWidth={1}
+      schPinArrangement={{
+        rightSide: {
+          pins: [1, 2, 3, 4],
+          direction: "bottom-to-top",
+        },
+      }}
+    />
+    <netlabel
+      schX="1.3"
+      schY="0.3"
+      anchorSide="left"
+      net="SCL"
+      connection={sel.U1.pin4}
+    />
+    <netlabel
+      schX="1.3"
+      schY="0.1"
+      anchorSide="left"
+      net="SDA"
+      connection={sel.U1.pin3}
+    />
+    <netlabel
+      schX="1.1"
+      schY="0.5"
+      anchorSide="bottom"
+      net="V3_3"
+      connection={sel.U1.pin2}
+    />
+    <netlabel
+      schX="1.1"
+      schY="-0.5"
+      anchorSide="top"
+      net="GND"
+      connection={sel.U1.pin1}
+    />
+  </board>
+)
