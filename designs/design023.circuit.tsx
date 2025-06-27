27307eace89f415ae8560c2ce08e31322954d399
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2025-06-27 22:20:04 +0530

    add design023 (#26)
    * add design023
    
    * add
    
    * add

---
+++ designs/design023.circuit.tsx
@@ -0,0 +1,40 @@
+import { sel } from "tscircuit"
+
+export default () => (
+  <board width="10mm" height="10mm">
+    <netlabel
+      schX={6.505}
+      schY={1.3}
+      net="VCC"
+      connection="R2.pin2"
+      anchorSide="bottom"
+    />
+    <resistor
+      resistance="10k"
+      footprint="0603"
+      name="R2"
+      schX={6.505}
+      schY={0.5}
+      schRotation={90}
+      connections={{
+        pin1: sel.LED2.pin1,
+      }}
+    />
+    <led
+      name="LED2"
+      footprint="0603"
+      schRotation={270}
+      schY={-0.9}
+      schX={6.5}
+      color="red"
+      schDisplayValue="GREEN"
+    />
+    <netlabel
+      schX={6.505}
+      schY={-1.7}
+      net="GND"
+      anchorSide="top"
+      connection={sel.LED2.pin2}
+    />
+  </board>
+)
