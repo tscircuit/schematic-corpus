b95ece0e6c1409d3f80a34c765e8395a885e9fac
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2025-06-28 21:23:42 +0530

    add design027 (#33)
    * add design027
    
    * update

---
+++ designs/design027.circuit.tsx
@@ -0,0 +1,49 @@
+import { sel } from "tscircuit"
+
+export default () => (
+  <board width="10mm" height="10mm">
+    <jumper name="J1" schX={0} schY={-5} footprint="pinrow8" />
+    <netlabel
+      schX={1}
+      schY={-5.6}
+      net="VIN"
+      anchorSide="bottom"
+      connection={sel.J1.pin1}
+    />
+    <netlabel
+      schX={0.8}
+      schY={-6}
+      net="GND"
+      anchorSide="top"
+      connectsTo={[sel.J1.pin2, sel.J1.pin3]}
+    />
+    <netlabel
+      schX={0.8}
+      schY={-4.2}
+      net="V5"
+      anchorSide="bottom"
+      connection={sel.J1.pin4}
+    />
+    <netlabel
+      schX={1.2}
+      schY={-4.2}
+      net="V3_3"
+      anchorSide="bottom"
+      connectsTo={sel.J1.pin5}
+    />
+    <netlabel
+      schX={1.8}
+      schY={-4.2}
+      net="VCC"
+      anchorSide="bottom"
+      connectsTo={sel.J1.pin7}
+    />
+    <netlabel
+      schX={1.4}
+      schY={-4.7}
+      net="N_RESET"
+      anchorSide="left"
+      connection={sel.J1.pin6}
+    />
+  </board>
+)
