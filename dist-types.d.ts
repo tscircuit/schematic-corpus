7fcdb138f58b095e13937fe88f5961d5439eb35f
imrishabh18 <belikerishabh@gmail.com>
2025-07-02 18:12:43 +0530

    remove last check in workflow

--- dist-types.d.ts
+++ dist-types.d.ts
@@ -1,8 +1,3 @@
-declare module "dist/bundled-bpc-graphs.json" {
-  const value: Record<string, any>
-  export default value
-}
-
 declare module "dist/svg-vfs.js" {
   const value: Record<string, any>
   export default value
--- scripts/import-bundled-graph.ts
+++ scripts/import-bundled-graph.ts
@@ -1,2 +1,4 @@
-import bundledBpcGraphs from "dist/bundled-bpc-graphs.json" with { type: "json" }
+import bundledBpcGraphs from "dist/bundled-bpc-graphs.json" with {
+  type: "json",
+}
 export const test = bundledBpcGraphs
