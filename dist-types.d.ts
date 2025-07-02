1fe8a0b09d66e59b67f3fec560ecd990052a2b6e
imrishabh18 <belikerishabh@gmail.com>
2025-07-02 19:05:58 +0530

    the types are back

--- dist-types.d.ts
+++ dist-types.d.ts
@@ -1,3 +1,8 @@
+declare module "dist/bundled-bpc-graphs.json" {
+  const value: Record<string, any>
+  export default value
+}
+
 declare module "dist/svg-vfs.js" {
   const value: Record<string, any>
   export default value
