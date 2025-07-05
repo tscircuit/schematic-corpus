4d93e17e83ab9297765f3e1ec4b763d8d8ea03dd
seveibar <seveibar@gmail.com>
2025-07-05 11:57:38 -0700

    fix types

--- designs/design041.circuit.tsx
+++ designs/design041.circuit.tsx
@@ -7,14 +7,14 @@ export default () => (
       resistance="1k"
       schX={0}
       schY={0}
-      connections={{ pin1: sel.net.NET1, pin2: sel.C1.pin1 }}
+      connections={{ pin1: sel.net<"NET1">().NET1, pin2: sel.C1.pin1 }}
     />
     <capacitor
       name="C1"
       capacitance="0.1uF"
       schX={1.2}
       schY={0}
-      connections={{ pin1: sel.R1.pin2, pin2: sel.net.NET2 }}
+      connections={{ pin1: sel.R1.pin2, pin2: sel.net<"NET2">().NET2 }}
     />
   </board>
 )
