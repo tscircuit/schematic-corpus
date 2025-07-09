f2d82882970f9807fb17a3aa39e5cfd0700ae367
seveibar <seveibar@gmail.com>
2025-07-08 20:51:25 -0700

    export bpc graphs with no net label

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,6 @@
+export { default as corpusNoNetLabel } from "dist/bundled-bpc-graphs-no-net-label.json" with {
+  type: "json",
+}
 import corpus from "dist/bundled-bpc-graphs.json" with { type: "json" }
 
 export default corpus
