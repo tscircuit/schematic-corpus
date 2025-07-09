f2b40f3758990a5aad718e7305dac485a8dc2341
Severin Ibarluzea <seve700@gmail.com>
2025-07-09 15:52:39 -0700

    Rename SVG copy script

---
+++ scripts/copy-svgs-to-site.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env bun
+import { promises as fs } from "fs"
+import { join, dirname } from "path"
+
+async function main() {
+  const scriptDir = dirname(new URL(import.meta.url).pathname)
+  const snapshotsDir = join(scriptDir, "..", "designs", "__snapshots__")
+  const outDir = join(scriptDir, "..", "dist-site")
+  await fs.mkdir(outDir, { recursive: true })
+  const files = await fs.readdir(snapshotsDir)
+  for (const file of files) {
+    if (!file.endsWith(".svg")) continue
+    const m = file.match(/^(design\d+)\.circuit-schematic\.snap\.svg$/)
+    if (!m) continue
+    const destName = `${m[1]}.svg`
+    await fs.copyFile(join(snapshotsDir, file), join(outDir, destName))
+  }
+}
+
+main().catch((err) => {
+  console.error(err)
+  process.exit(1)
+})
