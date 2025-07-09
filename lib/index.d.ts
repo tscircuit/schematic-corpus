d442314cafb39a2626670a606e13958636c885f7
seveibar <seveibar@gmail.com>
2025-07-08 21:10:31 -0700

    update type export

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,6 @@
 import type { FixedBpcGraph } from "bpc-graph"
 
+export declare const corpusNoNetLabel: Record<string, FixedBpcGraph>
+
 declare const corpus: Record<string, FixedBpcGraph>
 export default corpus
