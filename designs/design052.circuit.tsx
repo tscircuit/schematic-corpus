8504e8b1b920f15168bdfe36a8365728ef9d4a8a
Mustafa Mulla <97171641+MustafaMulla29@users.noreply.github.com>
2025-07-11 21:11:46 +0530

    Remove `schPinstyle` from design52 (#80)
    * remove pinstyle
    
    * fixed
    
    * fix format

--- designs/design052.circuit.tsx
+++ designs/design052.circuit.tsx
@@ -10,14 +10,6 @@ export default () => (
           direction: "bottom-to-top",
         },
       }}
-      schPinStyle={{
-        pin3: {
-          marginBottom: 0.2,
-        },
-        pin2: {
-          marginBottom: 0.2,
-        },
-      }}
     />
     <resistor
       name="R3"
@@ -26,29 +18,34 @@ export default () => (
         pin2: sel.U4.pin2,
       }}
       schRotation={90}
-      schX={1.7}
+      schX={1.5}
       schY={-0.8}
     />
     <netlabel
       net="GND"
       connection="U4.pin1"
       anchorSide="top"
-      schX={1}
+      schX={0.8}
       schY={-0.7}
     />
-    <netlabel net="QWIIC_PWR" connection="U4.pin2" anchorSide="left" schX={2} />
+    <netlabel
+      net="QWIIC_PWR"
+      connection="U4.pin2"
+      anchorSide="left"
+      schX={1.8}
+    />
     <netlabel
       net="GND"
       connection="R3.pin1"
       anchorSide="top"
-      schX={1.7}
+      schX={1.5}
       schY={-1.7}
     />
     <netlabel
       net="UIN"
       connection="U4.pin3"
       anchorSide="bottom"
-      schX={1}
+      schX={0.8}
       schY={0.7}
     />
   </board>
