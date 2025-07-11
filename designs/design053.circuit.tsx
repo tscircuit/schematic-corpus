6aeeb4b321cc599d2be1657ff4d087fa5c810dbd
techmannih <manishchaudharyttt@gmail.com>
2025-07-11 15:36:48 +0530

    add new design055 to improve autolayout algorithm

--- designs/design053.circuit.tsx
+++ designs/design053.circuit.tsx
@@ -12,42 +12,42 @@ export default () => (
       }}
     />
     <netlabel
-      schX={1}
-      schY={-1}
+      schX={0.8}
+      schY={-0.8}
       anchorSide="top"
       net="GND"
       connection="U1.pin1"
     />
     <netlabel
-      schX={1}
+      schX={0.8}
       schY={1}
       net="VCC"
       anchorSide="bottom"
       connection="U1.pin2"
     />
     <netlabel
-      schX={1.5}
+      schX={1}
       schY={0.5}
       anchorSide="left"
       net="N_INT"
       connection="U1.pin6"
     />
     <netlabel
-      schX={1.5}
+      schX={1}
       schY={0.3}
       anchorSide="left"
       net="DISABLE"
       connection="U1.pin5"
     />
     <netlabel
-      schX={1.5}
+      schX={1}
       schY={0.1}
       anchorSide="left"
       net="SCL"
       connection="U1.pin4"
     />
     <netlabel
-      schX={1.5}
+      schX={1}
       schY={-0.1}
       anchorSide="left"
       net="SDL"
