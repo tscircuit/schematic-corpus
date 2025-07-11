b5b8d4a2db3518785bac3df3bd0d150ee1641d48
Abse2001 <abdaslam13.2.2001@gmail.com>
2025-07-11 11:48:58 +0200

    Add new design (design054) to improve autolayout  algorithm

--- designs/design054.circuit.tsx
+++ designs/design054.circuit.tsx
@@ -4,8 +4,8 @@ const manualEdits = {
     {
       selector: "C2",
       center: {
-        x: 0.8381474226825295,
-        y: -0.016566465969821087,
+        x: 0.8046091265445721,
+        y: 0.22938103904186713,
       },
       relative_to: "group_center",
     },
@@ -13,7 +13,23 @@ const manualEdits = {
       selector: "R1",
       center: {
         x: 0.0004896001122474038,
-        y: 2.0869174746833035,
+        y: 1.7440815586064047,
+      },
+      relative_to: "group_center",
+    },
+    {
+      selector: "SW3",
+      center: {
+        x: 0,
+        y: 0.4136389857014756,
+      },
+      relative_to: "group_center",
+    },
+    {
+      selector: "C1",
+      center: {
+        x: -0.8335861253449377,
+        y: 1,
       },
       relative_to: "group_center",
     },
@@ -46,23 +62,28 @@ export default () => (
     <netlabel
       net="ARTEMIS_RTS"
       anchorSide="right"
-      schX={-2}
+      schX={-1.8}
       schY={1}
       connectsTo={sel.C1.pin1}
     />
     <netlabel
       net="ARTEMIS_RESET"
       anchorSide="left"
-      schX={1}
+      schX={0.805}
       schY={1}
       connectsTo={[sel.C1.pin2, sel.C2.pin2]}
     />
-    <netlabel net="GND" anchorSide="top" schY={-1} connectsTo={sel.SW3.pin1} />
     <netlabel
       net="GND"
       anchorSide="top"
-      schY={-1}
-      schX={0.835}
+      schY={-0.6}
+      connectsTo={sel.SW3.pin1}
+    />
+    <netlabel
+      net="GND"
+      anchorSide="top"
+      schY={-0.6}
+      schX={0.805}
       connectsTo={sel.C2.pin1}
     />
     <netlabel
