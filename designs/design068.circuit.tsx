da63638fda2a335b911a071f0f7780107b8facf2
Mustafa Mulla <97171641+MustafaMulla29@users.noreply.github.com>
2025-07-23 14:59:39 +0000

    Added design68 to improve autorouting algo

---
+++ designs/design068.circuit.tsx
@@ -0,0 +1,43 @@
+import { sel } from "tscircuit"
+
+export default () => (
+  <board width="10mm" height="10mm">
+    <capacitor
+      name="C5"
+      capacitance="1nf"
+      schRotation={90}
+      schY={-0.8}
+      connections={{
+        pin2: sel.R20.pin2,
+      }}
+    />
+    <resistor
+      name="R20"
+      resistance="3.3k"
+      schX={-1}
+      connections={{
+        pin2: sel.R23.pin1,
+      }}
+    />
+    <netlabel net="DIR_IN" anchorSide="left" connection="R23.pin2" schX={3} />
+    <resistor name="R23" resistance="470" schX={2} />
+    <netlabel net="DIR_P" anchorSide="right" connection="R20.pin1" schX={-2} />
+    <diode
+      name="D7"
+      schRotation={90}
+      schX={1.1}
+      schY={-0.83}
+      connections={{
+        pin2: sel.R23.pin1,
+      }}
+      avalanche
+    />
+    <netlabel
+      net="GND"
+      anchorSide="top"
+      connectsTo={["C5.pin1", "D7.pin1"]}
+      schX={0.55}
+      schY={-1.6}
+    />
+  </board>
+)
