21a57b7479728be8d5571505792fa1c72c7beeb5
Mustafa Mulla <97171641+MustafaMulla29@users.noreply.github.com>
2025-07-25 17:23:48 +0530

    Added design71 to improve matchAdapt algo (#109)
    * Added design71 to improve matchAdapt algo
    
    * format fix

---
+++ designs/design071.circuit.tsx
@@ -0,0 +1,59 @@
+import { sel } from "tscircuit"
+export default () => (
+  <board width="10mm" height="10mm">
+    <chip
+      name="U1"
+      schPinArrangement={{
+        rightSide: {
+          direction: "bottom-to-top",
+          pins: [1, 2],
+        },
+      }}
+    />
+    <netlabel
+      net="GND"
+      anchorSide="top"
+      connection="U1.pin1"
+      schX={1}
+      schY={-0.5}
+    />
+    <capacitor
+      name="C3"
+      capacitance="0.1uf"
+      schRotation={90}
+      schX={1.8}
+      schY={0.3}
+      connections={{
+        pin2: sel.U1.pin2,
+      }}
+    />
+    <netlabel
+      net="GND"
+      anchorSide="top"
+      connection="C3.pin1"
+      schX={1.8}
+      schY={-0.5}
+    />
+    <netlabel
+      net="V1_8"
+      anchorSide="bottom"
+      connectsTo={["C4.pin2", "C3.pin2"]}
+      schX={2.8}
+      schY={1}
+    />
+    <capacitor
+      name="C4"
+      capacitance="10uf"
+      schRotation={90}
+      schX={2.8}
+      schY={0.3}
+    />
+    <netlabel
+      net="GND"
+      anchorSide="top"
+      connection="C4.pin1"
+      schX={2.8}
+      schY={-0.5}
+    />
+  </board>
+)
