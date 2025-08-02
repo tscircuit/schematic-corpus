268b64b4d13908a99724bc889da10638ee2a620a
seveibar <seveibar@gmail.com>
2025-08-01 19:10:31 -0700

    add packFirst variation support

---
+++ lib/codegen/selectSchematicWithFewestCrossings.ts
@@ -0,0 +1,30 @@
+import { runTscircuitCode } from "tscircuit"
+
+export const selectSchematicWithFewestCrossings = async (
+  circuitCodes: string[],
+): Promise<{ bestCircuitIndex: number; bestCircuitCode: string }> => {
+  const circuitJsons = []
+  for (const circuitCode of circuitCodes) {
+    const circuitJson = await runTscircuitCode(circuitCode)
+    circuitJsons.push(circuitJson)
+  }
+
+  let bestCircuitIndex = 0
+  let lowestCrossings = Infinity
+  for (let i = 0; i < circuitJsons.length; i++) {
+    const circuitJson = circuitJsons[i]!
+    let crossings = 0
+    for (const elm of circuitJson) {
+      if (elm.type === "schematic_trace") {
+        crossings += elm.edges.filter((e) => e.is_crossing).length
+      }
+    }
+
+    if (crossings < lowestCrossings) {
+      lowestCrossings = crossings
+      bestCircuitIndex = i
+    }
+  }
+
+  return { bestCircuitIndex, bestCircuitCode: circuitCodes[bestCircuitIndex]! }
+}
--- scripts/generate-3pin-designs.ts
+++ scripts/generate-3pin-designs.ts
@@ -8,6 +8,8 @@ import {
   trace,
 } from "lib/codegen/codegen-primitives"
 import { pack } from "calculate-packing"
+import { runTscircuitCode } from "tscircuit"
+import { selectSchematicWithFewestCrossings } from "lib/codegen/selectSchematicWithFewestCrossings"
 
 async function main() {
   const scriptDir = dirname(new URL(import.meta.url).pathname)
@@ -21,130 +23,146 @@ async function main() {
     for (const cPin of pins) {
       if (cPin === rPin) continue // R1 & C1 can’t share the same pin
 
-      const packOutput = pack({
-        components: [
-          {
-            componentId: "U1",
-            pads: pins
-              .map((p) => ({
-                networkId: `net_U1.pin${p}`,
-                offset: { x: 0.5, y: 0.2 * p - 0.2 },
-                padId: `pad_U1.pin${p}`,
-                size: {
-                  x: 0.15,
-                  y: 0.15,
-                },
-                type: "rect" as const,
-              }))
-              .concat([
+      const packOrderVariationsToTest = [
+        ["U1", "R1", "C1"],
+        ["U1", "C1", "R1"],
+      ]
+
+      const possibleGeneratedCircuitCodes = []
+
+      for (const packFirstOrder of packOrderVariationsToTest) {
+        const packOutput = pack({
+          components: [
+            {
+              componentId: "U1",
+              pads: pins
+                .map((p) => ({
+                  networkId: `net_U1.pin${p}`,
+                  offset: { x: 0.5, y: 0.2 * p - 0.2 },
+                  padId: `pad_U1.pin${p}`,
+                  size: {
+                    x: 0.15,
+                    y: 0.15,
+                  },
+                  type: "rect" as const,
+                }))
+                .concat([
+                  {
+                    networkId: "net_U1.KEEPOUT",
+                    offset: { x: 0, y: 0 },
+                    padId: "pad_U1.KEEPOUT",
+                    size: { x: 0.3, y: 2 },
+                    type: "rect",
+                  },
+                ]),
+            },
+            {
+              componentId: "R1",
+              pads: [
                 {
-                  networkId: "net_U1.KEEPOUT",
-                  offset: { x: 0, y: 0 },
-                  padId: "pad_U1.KEEPOUT",
-                  size: { x: 0.3, y: 2 },
+                  networkId: `net_U1.pin${rPin}`,
+                  offset: { x: -0.3, y: 0 },
+                  padId: "pad_R1.pin1",
+                  size: { x: 0.15, y: 0.15 },
                   type: "rect",
                 },
-              ]),
-          },
-          {
-            componentId: "R1",
-            pads: [
-              {
-                networkId: `net_U1.pin${rPin}`,
-                offset: { x: -0.3, y: 0 },
-                padId: "pad_R1.pin1",
-                size: { x: 0.15, y: 0.15 },
-                type: "rect",
-              },
-              {
-                networkId: "net_R1.pin2",
-                offset: { x: 0.3, y: 0 },
-                padId: "pad_R1.pin2",
-                size: { x: 0.15, y: 0.15 },
-                type: "rect",
-              },
-              {
-                networkId: "net_R1.KEEPOUT",
-                offset: { x: 0, y: 0 },
-                padId: "pad_R1.KEEPOUT",
-                size: { x: 1, y: 0.5 },
-                type: "rect",
-              },
-            ],
-          },
-          {
-            componentId: "C1",
-            pads: [
-              {
-                networkId: `net_U1.pin${cPin}`,
-                offset: { x: -0.3, y: 0 },
-                padId: "pad_C1.pin1",
-                size: { x: 0.25, y: 0.25 },
-                type: "rect",
-              },
-              {
-                networkId: "net_C1.pin2",
-                offset: { x: 0.3, y: 0 },
-                padId: "pad_C1.pin2",
-                size: { x: 0.25, y: 0.25 },
-                type: "rect",
-              },
-              {
-                networkId: "net_C1.KEEPOUT",
-                offset: { x: 0, y: 0 },
-                padId: "pad_C1.KEEPOUT",
-                size: { x: 1, y: 0.5 },
-                type: "rect",
-              },
-            ],
-          },
-        ],
-        minGap: 0.1,
-        packOrderStrategy: "largest_to_smallest",
-        packPlacementStrategy: "shortest_connection_along_outline",
-      })
+                {
+                  networkId: "net_R1.pin2",
+                  offset: { x: 0.3, y: 0 },
+                  padId: "pad_R1.pin2",
+                  size: { x: 0.15, y: 0.15 },
+                  type: "rect",
+                },
+                {
+                  networkId: "net_R1.KEEPOUT",
+                  offset: { x: 0, y: 0 },
+                  padId: "pad_R1.KEEPOUT",
+                  size: { x: 1, y: 0.5 },
+                  type: "rect",
+                },
+              ],
+            },
+            {
+              componentId: "C1",
+              pads: [
+                {
+                  networkId: `net_U1.pin${cPin}`,
+                  offset: { x: -0.3, y: 0 },
+                  padId: "pad_C1.pin1",
+                  size: { x: 0.25, y: 0.25 },
+                  type: "rect",
+                },
+                {
+                  networkId: "net_C1.pin2",
+                  offset: { x: 0.3, y: 0 },
+                  padId: "pad_C1.pin2",
+                  size: { x: 0.25, y: 0.25 },
+                  type: "rect",
+                },
+                {
+                  networkId: "net_C1.KEEPOUT",
+                  offset: { x: 0, y: 0 },
+                  padId: "pad_C1.KEEPOUT",
+                  size: { x: 1, y: 0.5 },
+                  type: "rect",
+                },
+              ],
+            },
+          ],
+          minGap: 0.1,
+          packOrderStrategy: "largest_to_smallest",
+          packPlacementStrategy: "shortest_connection_along_outline",
+          packFirst: packFirstOrder,
+        })
 
-      const packPlacements: Record<
-        string,
-        { x: number; y: number; ccwRotationDegrees: number }
-      > = {}
-      for (const c of packOutput.components) {
-        packPlacements[c.componentId] = {
-          x: c.center.x,
-          y: c.center.y,
-          ccwRotationDegrees: (c.ccwRotationOffset / Math.PI) * 180,
+        const packPlacements: Record<
+          string,
+          { x: number; y: number; ccwRotationDegrees: number }
+        > = {}
+        for (const c of packOutput.components) {
+          packPlacements[c.componentId] = {
+            x: c.center.x,
+            y: c.center.y,
+            ccwRotationDegrees: (c.ccwRotationOffset / Math.PI) * 180,
+          }
         }
+
+        const circuitCode = makeCircuit([
+          u1Chip({
+            pinCount: 3,
+            connections: {},
+            schX: packPlacements["U1"]?.x,
+            schY: packPlacements["U1"]?.y,
+            schRotation: packPlacements["U1"]?.ccwRotationDegrees,
+          }),
+          resistor({
+            name: "R1",
+            schX: packPlacements["R1"]?.x,
+            schY: packPlacements["R1"]?.y,
+            schRotation: packPlacements["R1"]?.ccwRotationDegrees,
+          }),
+          capacitor({
+            name: "C1",
+            schX: packPlacements["C1"]?.x,
+            schY: packPlacements["C1"]?.y,
+            schRotation: packPlacements["C1"]?.ccwRotationDegrees,
+          }),
+          trace({ from: "R1.pin1", to: `U1.pin${rPin}` }),
+          trace({ from: "R1.pin2", to: "net.VCC" }),
+          trace({ from: "C1.pin1", to: `U1.pin${cPin}` }),
+          trace({ from: "C1.pin2", to: "net.GND" }),
+        ])
+        possibleGeneratedCircuitCodes.push(
+          `export default () => (\n${circuitCode}\n)\n`,
+        )
       }
 
-      const circuit = makeCircuit([
-        u1Chip({
-          pinCount: 3,
-          connections: {},
-          schX: packPlacements["U1"]?.x,
-          schY: packPlacements["U1"]?.y,
-          schRotation: packPlacements["U1"]?.ccwRotationDegrees,
-        }),
-        resistor({
-          name: "R1",
-          schX: packPlacements["R1"]?.x,
-          schY: packPlacements["R1"]?.y,
-          schRotation: packPlacements["R1"]?.ccwRotationDegrees,
-        }),
-        capacitor({
-          name: "C1",
-          schX: packPlacements["C1"]?.x,
-          schY: packPlacements["C1"]?.y,
-          schRotation: packPlacements["C1"]?.ccwRotationDegrees,
-        }),
-        trace({ from: "R1.pin1", to: `U1.pin${rPin}` }),
-        trace({ from: "R1.pin2", to: "net.VCC" }),
-        trace({ from: "C1.pin1", to: `U1.pin${cPin}` }),
-        trace({ from: "C1.pin2", to: "net.GND" }),
-      ])
+      const { bestCircuitCode } = await selectSchematicWithFewestCrossings(
+        possibleGeneratedCircuitCodes,
+      )
 
-      const fileContents = `export default () => (\n${circuit}\n)\n`
       const fileName = `pin3set${String(idx).padStart(3, "0")}.circuit.tsx`
-      await fs.writeFile(join(outDir, fileName), fileContents)
+      await fs.writeFile(join(outDir, fileName), bestCircuitCode)
       idx++
     }
   }
